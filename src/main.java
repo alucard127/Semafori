63fae6d5f769f1bdcb2e4ae3d169a600fb5f0ea3
alucard127 <goldenalucard@gmail.com>
2021-08-19 14:14:51 -0400

    second commit

--- src/main.java
+++ src/main.java
@@ -1,33 +1,36 @@
 import java.util.Scanner;
-public class main {
 
+public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N;
-        int D;
+        int L;
+        System.out.println("Enter Times: ");
         N = scan.nextInt();
-        D = scan.nextInt();
+        L = scan.nextInt();
 
         int x = 0;
         int t = 0;
-        for (int i = 0; i < N; i++) {
-            int L;
-            int C;
-            int Z;
-            L =  scan.nextInt();
-            C =  scan.nextInt();
-            Z =  scan.nextInt();
-
-            t += L - x;
-            x = L;
-            while (t % (C + Z) < C) {
-                System.out.println("TIEMPO: "+t);
-                ++t;
+        for (int i = 0; i < N; i++)
+        {
+            int D;
+            int R;
+            int G;
+            D =  scan.nextInt();
+            R =  scan.nextInt();
+            G =  scan.nextInt();
+            t += D - x;
+            x = D;
+            while (t % (R + G) < R)
+            {
+                t++;
             }
         }
-        t += D - x;
-        x = D;
-        System.out.printf ("%d\n", t);
+        t += L - x;
+        x = L;
+
+        System.out.println(t);
 
     }
+
 }
